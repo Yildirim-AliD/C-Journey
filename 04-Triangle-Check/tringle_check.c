52a657cf7580fdcd393c10e4ce27be82d3c5ea95
Yildirim-AliD <yldrmalid@gmail.com>
2025-03-01 01:21:19 +0300

    04-Tringle-Check

--- 04-Triangle-Check/tringle_check.c
+++ 04-Triangle-Check/tringle_check.c
@@ -4,18 +4,18 @@
 int main()
 {
     float num1,num2,num3;
-    printf("3 değer giriniz: ");
+    printf("Please Enter 3 values: ");
     scanf("%f %f %f",&num1,&num2,&num3);
     
     if(num1+num2>num3 && num3 > fabs(num1-num2) &&
     num1+num3>num2 && num2 > fabs(num3-num1) &&
     num3+num2>num1 && num1 > fabs(num2-num3))
     {
-        printf("evet üçgendir\n");
+        printf("Yes,a triangle can be formed with these 3 values\n");
         return 0;
         
     }else{
-        printf("hayır üçgen değildir\n");
+        printf("No,a triangle cannot be formed with these values\n");
         return 1;
     }
 }
